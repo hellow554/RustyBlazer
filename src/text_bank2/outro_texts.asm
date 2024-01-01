a031a9ac4e120da21108309be405347eac6732b7
Marcel Hellwig <git@cookiesoft.de>
2024-01-01 10:24:28 +0100

    move outro text into own file

---
+++ src/text_bank2/outro_texts.asm
@@ -0,0 +1,98 @@
+
+; @NEW_TEXT@
+; SETPOS $40 $02
+; "  Grass Valley bei Sonnen-\n\n"
+; "  aufgang. Der schäumende \n\n"
+; "  Ozean. Die wiederbelebte \n\n"
+; "  Welt war wunderschön...." NO_NEWLINE
+db $01,$40,$02
+db $20,$20,$47,$72,$61,$73,$73,$20,$56,$61,$6C,$6C,$65,$79,$20,$62,$65,$69,$20,$53,$6F,$6E,$6E,$65,$6E,$2D,$0D,$0D,$0D
+db $20,$20,$61,$75,$66,$67,$61,$6E,$67,$2E,$20,$88,$73,$63,$68,$7B,$75,$6D,$65,$6E,$64,$65,$20,$0D,$0D,$0D
+db $20,$20,$4F,$7A,$65,$61,$6E,$2E,$20,$87,$77,$69,$65,$64,$65,$72,$62,$65,$6C,$65,$62,$74,$65,$20,$0D,$0D,$0D
+db $20,$20,$A9,$FC,$77,$75,$6E,$64,$65,$72,$73,$63,$68,$2A,$6E,$2E,$2E,$2E,$2E
+db $00
+; @ENDSTRING@
+
+; @NEW_TEXT@
+; SETPOS $C0 $02
+; SPACE 3 "Alle Lebewesen erfreuten \n\n"
+; SPACE 3 "sich an der Schönheit des \n\n"
+; SPACE 3 "Sonnenaufganges." NO_NEWLINE
+db $01,$C0,$02
+db $14,$03,$41,$6C,$6C,$65,$20,$4C,$65,$62,$65,$77,$65,$73,$65,$6E,$20,$65,$72,$66,$72,$65,$75,$74,$65,$6E,$20,$0D,$0D,$0D
+db $14,$03,$E5,$61,$6E,$20,$B6,$53,$63,$68,$2A,$6E,$68,$65,$69,$74,$20,$B8,$0D,$0D,$0D
+db $14,$03,$53,$6F,$6E,$6E,$65,$6E,$61,$75,$66,$67,$61,$6E,$67,$65,$73,$2E
+db $00
+; @ENDSTRING@
+
+; @NEW_TEXT@
+; SETPOS $40 $02
+; " Jemand hatte sein Kind dabei \n\n"
+; " ein anderer seinen Freund \n\n"
+; " die Kreaturen waren ver-\n\n"
+; " zaubert vom Anblick." NO_NEWLINE
+db $01,$40,$02
+db $20,$4A,$65,$6D,$61,$6E,$64,$20,$68,$61,$74,$74,$65,$20,$73,$65,$69,$6E,$20,$4B,$69,$6E,$64,$20,$64,$61,$62,$65,$69,$20,$0D,$0D,$0D
+db $20,$BE,$61,$6E,$64,$65,$72,$65,$72,$20,$73,$65,$69,$6E,$65,$6E,$20,$46,$72,$65,$75,$6E,$64,$20,$0D,$0D,$0D
+db $20,$B5,$4B,$72,$65,$61,$74,$75,$72,$65,$6E,$20,$77,$61,$72,$65,$6E,$20,$76,$65,$72,$2D,$0D,$0D,$0D
+db $20,$7A,$61,$75,$62,$65,$72,$74,$20,$F1,$41,$6E,$62,$6C,$69,$63,$6B,$2E
+db $00
+; @ENDSTRING@
+
+; @NEW_TEXT@
+; SETPOS $C0 $02
+; "  Plötzlich schoß ein Licht-\n\n"
+; "  strahl in den Himmel.\n\n"
+; "  Trotzdem niemand sah es." NO_NEWLINE
+db $01,$C0,$02
+db $20,$20,$50,$6C,$2A,$74,$7A,$6C,$69,$63,$68,$20,$73,$63,$68,$6F,$25,$20,$BE,$4C,$69,$63,$68,$74,$2D,$0D,$0D,$0D
+db $20,$20,$73,$74,$72,$61,$68,$6C,$20,$D3,$B7,$48,$69,$6D,$6D,$65,$6C,$2E,$0D,$0D,$0D
+db $20,$20,$54,$72,$6F,$74,$7A,$64,$65,$6D,$20,$6E,$69,$65,$6D,$61,$6E,$64,$20,$73,$61,$68,$20,$65,$73,$2E
+db $00
+; @ENDSTRING@
+
+; @NEW_TEXT@
+; SETPOS $C0 $02
+; SPACE 3 "Lisa stand dort ganz \n\n"
+; SPACE 3 "alleine." NO_NEWLINE
+db $01,$C0,$02
+db $14,$03,$94,$73,$74,$61,$6E,$64,$20,$64,$6F,$72,$74,$20,$67,$61,$6E,$7A,$20,$0D,$0D,$0D
+db $14,$03,$61,$6C,$6C,$65,$69,$6E,$65,$2E
+db $00
+; @ENDSTRING@
+
+; @NEW_TEXT@
+; SETPOS $40 02
+; SPACE 3 "Früher fühlte sie sich ein-\n\n"
+; SPACE 3 "sam, wenn sie den Sonnen-\n\n"
+; SPACE 3 "aufgang sah aber jetzt \n\n"
+; SPACE 3 "hält sie ihn für schön." NO_NEWLINE
+db $01,$40,$02
+db $14,$03,$46,$72,$5B,$68,$65,$72,$20,$66,$5B,$68,$6C,$74,$65,$20,$E6,$E5,$65,$69,$6E,$2D,$0D,$0D,$0D
+db $14,$03,$73,$61,$6D,$2C,$20,$F3,$E6,$B7,$53,$6F,$6E,$6E,$65,$6E,$2D,$0D,$0D,$0D
+db $14,$03,$61,$75,$66,$67,$61,$6E,$67,$20,$73,$61,$68,$20,$AB,$D6,$0D,$0D,$0D
+db $14,$03,$68,$7B,$6C,$74,$20,$E6,$69,$68,$6E,$20,$C9,$73,$63,$68,$2A,$6E,$2E
+db $00
+; @ENDSTRING@
+
+; @NEW_TEXT@
+; SETPOS $40 $02
+; "  So wie ihr Vater das \n\n"
+; "  Unmögliche möglich machte \n\n"
+; "  glaubt Lisa an ihr Glück." NO_NEWLINE
+db $01,$40,$02
+db $20,$20,$53,$6F,$20,$F6,$69,$68,$72,$20,$56,$61,$74,$65,$72,$20,$B9,$0D,$0D,$0D
+db $20,$20,$55,$6E,$6D,$2A,$67,$6C,$69,$63,$68,$65,$20,$6D,$2A,$67,$6C,$69,$63,$68,$20,$6D,$61,$63,$68,$74,$65,$20,$0D,$0D,$0D
+db $20,$20,$67,$6C,$61,$75,$62,$74,$20,$94,$61,$6E,$20,$69,$68,$72,$20,$47,$6C,$5B,$63,$6B,$2E
+db $00
+; @ENDSTRING@
+
+; @NEW_TEXT@
+; CURSIVE
+; SETPOS $58 $03
+; "Das Ende" NO_NEWLINE
+db $09
+db $01,$58,$03
+db $8A,$45,$6E,$64,$65
+db $00
+; @ENDSTRING@
